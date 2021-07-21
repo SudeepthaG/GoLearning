36b840b4f3a39cc0c3a3eb0e03cccd12315a23d8
Sudeeptha Mouni Ganji <32644423+SudeepthaG@users.noreply.github.com>
2021-07-21 15:58:41 -0500

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/SudeepthaG/golearn
+
+go 1.16
---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+
+	functionspackages "github.com/SudeepthaG/golearn/FunctionsPackages"
+	// helloworld "github.com/SudeepthaG/golearn/HelloWorld"
+	// variablesconstantstypes "github.com/SudeepthaG/golearn/VariablesConstantsTypes"
+)
+
+func main() {
+
+	// fmt.Println(helloworld.Hello())
+	// fmt.Println(variablesconstantstypes.VariablesFunc())
+	// fmt.Println(variablesconstantstypes.ConstantsFunc())
+
+	fmt.Println(functionspackages.FunctionUsage())
+
+}
