cfaf47d11ab76f20229b46273357e35331918294
Sudeeptha Mouni Ganji <32644423+SudeepthaG@users.noreply.github.com>
2021-07-23 00:42:21 -0500

    Update main.go

--- main.go
+++ main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"fmt"
-
-	functionspackages "github.com/SudeepthaG/golearn/FunctionsPackages"
+	mapsstrings "github.com/SudeepthaG/golearn/MapsStrings"
 	// helloworld "github.com/SudeepthaG/golearn/HelloWorld"
 	// variablesconstantstypes "github.com/SudeepthaG/golearn/VariablesConstantsTypes"
 )
@@ -14,6 +12,11 @@ func main() {
 	// fmt.Println(variablesconstantstypes.VariablesFunc())
 	// fmt.Println(variablesconstantstypes.ConstantsFunc())
 
-	fmt.Println(functionspackages.FunctionUsage())
+	// fmt.Println(functionspackages.FunctionUsage())
+	// fmt.Println(mapsstrings.Maps())
+	// mapsstrings.Maps()				//if there was no return
+	// mapsstrings.Summain()
+	mapsstrings.StringPractice()
 
 }
+
